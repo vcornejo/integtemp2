b113b0496b4e8004ab08407f270d2902f967d340
vic <victor@redhat.com>
2018-09-12 01:19:19 +0000

    Modificación del mensaje

--- src/main/java/io/openshift/booster/service/Greeting.java
+++ src/main/java/io/openshift/booster/service/Greeting.java
@@ -18,7 +18,7 @@ package io.openshift.booster.service;
 // tag::snippet-greeting[]
 public class Greeting {
 
-    public static final String FORMAT = "Hello, %s!";
+    public static final String FORMAT = "Hola. Bienvenidos Inacap , %s!";
 
     private final String content;
 
